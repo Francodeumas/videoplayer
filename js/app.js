c0a74aac6eff870b6bf566f0f3e3e853f2137915
Franco <franco.deumas1990@gmail.com>
2017-02-21 20:29:31 +0530

    toggle Video play and pause

--- js/app.js
+++ js/app.js
@@ -4,16 +4,26 @@
   
   var logics = {
   
+    toggleVideo : function(){
+      var video = document.getElementsByTagName('video');
+          if(video[0].paused)
+            video[0].play();
+          else if(video[0].played)
+            video[0].pause();
+    },
+    
     switchPlayButton : function(){
        var playIconDisplay = window.getComputedStyle(document.getElementById('play_icon'),null).getPropertyValue('display'),
            pauseIconDisplay = window.getComputedStyle(document.getElementById('pause_icon'),null).getPropertyValue('display');
           if(pauseIconDisplay === "none"){
             document.getElementById('play_icon').style.display = "none";
             document.getElementById('pause_icon').style.display = "inline-block";
+            this.toggleVideo();      
           }
          else {
            document.getElementById('play_icon').style.display = "inline-block";
            document.getElementById('pause_icon').style.display = "none";
+           this.toggleVideo()
          }
     }
     
