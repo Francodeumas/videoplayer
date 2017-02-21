57634a072a62c4bf6e2c35d37b17fdfbafbe2401
Franco <franco.deumas1990@gmail.com>
2017-02-21 23:09:14 +0530

    toggle volume and mute

--- js/app.js
+++ js/app.js
@@ -16,15 +16,48 @@
        var playIconDisplay = window.getComputedStyle(document.getElementById('play_icon'),null).getPropertyValue('display'),
            pauseIconDisplay = window.getComputedStyle(document.getElementById('pause_icon'),null).getPropertyValue('display');
           if(pauseIconDisplay === "none"){
+            this.toggleVideo(); 
             document.getElementById('play_icon').style.display = "none";
             document.getElementById('pause_icon').style.display = "inline-block";
-            this.toggleVideo();      
           }
          else {
+           this.toggleVideo();
            document.getElementById('play_icon').style.display = "inline-block";
            document.getElementById('pause_icon').style.display = "none";
-           this.toggleVideo()
+           
          }
+    },
+    
+    toggleScreen : function(){
+      var container = document.getElementById('video');
+      if(container.requestFullScreen){
+        container.requestFullScreen();
+      }
+      else if(container.webkitRequestFullScreen){
+        container.webkitRequestFullScreen();
+      }
+      else if(container.mozRequestFullScreen){
+        container.mozRequestFullScreen();
+      }
+      else if(container.msRequestFullscreen){
+        container.msRequestFullscreen();
+      }
+    },
+    
+    toggleVolume : function(){
+      var volumeDisplay = window.getComputedStyle(document.getElementById('volume_icon'),null).getPropertyValue('display'),
+          muteDisplay  = window.getComputedStyle(document.getElementById('mute_icon'),null).getPropertyValue('display');
+      
+       if(volumeDisplay === "none"){
+         document.getElementById('volume_icon').style.display = "inline-block";
+         document.getElementById('mute_icon').style.display = "none";
+         document.getElementsByTagName('video')[0].muted = false;
+       }
+      else{
+        document.getElementById('volume_icon').style.display = "none";
+        document.getElementById('mute_icon').style.display = "inline-block";
+        document.getElementsByTagName('video')[0].muted = true;
+      }
     }
     
   }
@@ -37,6 +70,12 @@
            if(event.target.id === "play_icon" || event.target.id === "pause_icon"){
               logics.switchPlayButton();
            }
+           else if(event.target.id === "maximize_icon"){
+              logics.toggleScreen();
+           }
+          else if(event.target.id === "volume_icon" || event.target.id === "mute_icon"){
+              logics.toggleVolume();
+          }
         })
       }
    }
